9338173dac0a3cc3b08494631c48967aba2cc661
Zoe Larsen <zlarsen.zl@gmail.co>
2023-05-14 12:10:34 -0600

    pagination hiding

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,6 +4,11 @@ class BlogPostsController < ApplicationController
   def index
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     @pagy, @blog_posts = pagy(@blog_posts)
+  rescue Pagy::OverflowError
+    redirect_to root_path(page: 1)
+    ## alternative way to handle rescue
+    # params[:page] = 1
+    # retry
   end
   def show
 
