d0e6360fc2ea01045a6c22f4aed42a80bbf81adb
Zoe Larsen <zlarsen.zl@gmail.co>
2023-05-22 12:28:09 -0500

    some css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :blog_posts do
     resource :cover_image, only: [:destroy], module: :blog_posts
   end
+
+  get "/landing_page", to: "landing_pages#index", as: :landing_page
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
